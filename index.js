c053837aca984c5cdefc7c98f6d7ffc67128ec0a
Karl Young <Karlyoung112@gmail.com>
2024-04-10 23:13:22 +1200

    Rewrote the code same problem i will continue with broken code for now

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let canvas, ctx
+let canvas
+let ctx
 let canvasWidth = 1400
 let canvasHeight = 1000
 let keys = []
@@ -12,17 +13,14 @@ function SetupCanvas() {
   canvas.height = canvasHeight
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+
   document.body.addEventListener('keydown', function (e) {
     keys[e.key] = true
-    ship.movingForward = true
-    console.log('Key pressed:', e.key)
   })
   document.body.addEventListener('keyup', function (e) {
     keys[e.key] = false
-    ship.movingForward = false
-    console.log('Key unpressed:', e.key)
   })
-  canvas.focus()
+
   Render()
 }
 
@@ -31,83 +29,85 @@ class Ship {
     this.visible = true
     this.x = canvasWidth / 2
     this.y = canvasHeight / 2
+    this.movingForward = false
     this.speed = 0.1
-    this.velx = 0
-    this.vely = 0
-    this.rotateSpeed = 0.001
+    this.velX = 0
+    this.velY = 0
+    this.rotateSpeed = 0.004
     this.radius = 15
     this.angle = 0
     this.strokeColor = 'white'
   }
-
   Rotate(dir) {
     this.angle += this.rotateSpeed * dir
   }
   Update() {
-    // console.log('Updating ship...')
-    // console.log('this.movingForward:', this.movingForward)
+    // Get current direction ship is facing
     let radians = (this.angle / Math.PI) * 180
 
+    // If ship is moving forward
     if (this.movingForward) {
-      this.velx += Math.cos(radians) * this.speed
-      this.vely += Math.sin(radians) * this.speed
-      console.log('Ship velocity X:', this.velx)
-      console.log('Ship velocity Y:', this.vely)
+      this.velX += Math.cos(radians) * this.speed
+      this.velY += Math.sin(radians) * this.speed
     }
-
-    // Update ship position based on velocity
-    this.x += this.velx
-    this.y += this.vely
-
-    // Wrap around the canvas if ship goes out of bounds
-    if (this.x > canvas.width + this.radius) {
-      this.x = -this.radius
-    } else if (this.x < -this.radius) {
-      this.x = canvas.width + this.radius
+    // If ship goes off board place it on the opposite
+    // side
+    if (this.x < this.radius) {
+      this.x = canvas.width
     }
-    if (this.y > canvas.height + this.radius) {
-      this.y = -this.radius
-    } else if (this.y < -this.radius) {
-      this.y = canvas.height + this.radius
+    if (this.x > canvas.width) {
+      this.x = this.radius
     }
+    if (this.y < this.radius) {
+      this.y = canvas.height
+    }
+    if (this.y > canvas.height) {
+      this.y = this.radius
+    }
+    // Slow ship speed when not holding key
+    this.velX *= 0.89
+    this.velY *= 0.89
 
-    // Apply friction to slow down the ship
-    this.velx *= 0.99
-    this.vely *= 0.99
+    // Change value of x & y while accounting for
+    // air friction
+    this.x -= this.velX
+    this.y -= this.velY
   }
+
   Draw() {
     ctx.strokeStyle = this.strokeColor
     ctx.beginPath()
-    let angle = this.angle
-    let x = this.x
-    let y = this.y
-    ctx.moveTo(
-      x + this.radius * Math.cos(angle),
-      y + this.radius * Math.sin(angle)
-    )
-    ctx.lineTo(
-      x + this.radius * Math.cos(angle - (Math.PI * 2) / 3),
-      y + this.radius * Math.sin(angle - (Math.PI * 2) / 3)
-    )
-    ctx.lineTo(
-      x + this.radius * Math.cos(angle + (Math.PI * 2) / 3),
-      y + this.radius * Math.sin(angle + (Math.PI * 2) / 3)
-    )
+    // Angle between vertices of the ship
+    let vertAngle = (Math.PI * 2) / 3
+
+    let radians = (this.angle / Math.PI) * 180
+
+    for (let i = 0; i < 3; i++) {
+      ctx.lineTo(
+        this.x - this.radius * Math.cos(vertAngle * i + radians),
+        this.y - this.radius * Math.sin(vertAngle * i + radians)
+      )
+    }
     ctx.closePath()
     ctx.stroke()
   }
 }
 
 let ship = new Ship()
+
 function Render() {
+  // Check if the ship is moving forward
   ship.movingForward = keys[87]
+
   if (keys[68]) {
-    console.log(keys[68])
+    // d key rotate right
     ship.Rotate(1)
   }
   if (keys[65]) {
+    // a key rotate left
     ship.Rotate(-1)
   }
+
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
   ship.Update()
   ship.Draw()
